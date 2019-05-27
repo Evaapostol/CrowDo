dbfe956c6624539ae06bcbb842aff55cde7214b2
Eugenia Apostol <50484662+Evaapostol@users.noreply.github.com>
2019-05-27 14:43:31 +0300

    Add files via upload

---
+++ CrowDo1st/ProjectCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowDo1st
+{
+    public class ProjectCategory
+    {
+        public int ProjectId { get; set; }
+        public ProjectProfilePage Project { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public ProjectCategory()
+        {
+        }
+    }
+}
---
+++ CrowDo1st/UserProject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowDo1st
+{
+    public class UserProject 
+    {
+        public int UserId { get; set; }
+        public User user { get; set; }
+        public int ProjectId { get; set; }
+        public ProjectProfilePage Project { get; set; }
+        
+        public UserProject()
+        {
+
+        }
+    }
+
+}
